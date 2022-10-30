99042d9c1b63e97c8f3b6da002383c45dc8d761c
Deven Gangwani <devengangwani12@gmail.com>
2022-10-30 17:49:06 +0530

    Added firefox option to main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import sys
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+
 with  open("cred.txt","r") as f:
     cred = f.read()
 with  open("recv.txt","r") as f:
@@ -12,14 +13,24 @@ name = credarr[0];
 passwd = credarr[1];
 recv_list = recv.split("\n")
 subject = "Subject"
-driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
+
+
+if (sys.argv[1] == "firefox"):	
+	from selenium.webdriver.firefox.options import Options as FFOptions
+	options = FFOptions()
+	driver = webdriver.Firefox(options=options)
+else:
+	from selenium.webdriver.chrome.service import Service as ChromeService
+	from webdriver_manager.chrome import ChromeDriverManager
+	driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
+
+driver.implicitly_wait(20)
+
 driver.get("https://nwm.iitk.ac.in/")
+
 driver.find_element(by = By.ID , value="rcmloginuser").send_keys(name)
-sleep(1)
 driver.find_element(by = By.ID , value="rcmloginpwd").send_keys(passwd)
-sleep(1)
 driver.find_element(by = By.ID , value="rcmloginsubmit").click()
-sleep(1)
 for to in recv_list :
     driver.find_element(by = By.ID , value="rcmbtn109").click()
     sleep(1)
