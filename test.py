fe2a287247818add315e59828f0a6aada205f293
Deven Gangwani <devengangwani12@gmail.com>
2022-10-30 18:03:06 +0530

    started work on webmail automation

--- test.py
+++ test.py
@@ -1,3 +1,30 @@
 import sys
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
-print(sys.argv)
+with  open("cred.txt","r") as f:
+    cred = f.read()
+with  open("recv.txt","r") as f:
+    recv = f.read()
+credarr = cred.split("\n")
+name = credarr[0];
+passwd = credarr[1];
+recv_list = recv.split("\n")
+subject = "Subject"
+
+
+if (sys.argv[1] == "firefox"):	
+	from selenium.webdriver.firefox.options import Options as FFOptions
+	options = FFOptions()
+	driver = webdriver.Firefox(options=options)
+else:
+	from selenium.webdriver.chrome.service import Service as ChromeService
+	from webdriver_manager.chrome import ChromeDriverManager
+	driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
+
+driver.get("https://webmail.iitk.ac.in/squirrelmail/src/login.php")
+
+captcha = driver.find_element(by = By.CSS_SELECTOR, "table center img")
+captcha.save_screenshot("captcha.png")
